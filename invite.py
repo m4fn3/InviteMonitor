7a73eda5eba8a3302b04b41ff21aa30d8f208bf7
mafusuke <mafusukee@gmail.com>
2020-10-08 00:20:31 +0900

    Add emoji to clear commands

--- invite.py
+++ invite.py
@@ -196,14 +196,14 @@ class Invite(commands.Cog):
             await ctx.send(f"{self.bot.datas['emojis']['loading']} It may takes several time if the server is large..")
             for invite in await ctx.guild.invites():
                 await invite.delete()
-            await ctx.send("All server invites has deleted successfully!")
+            await ctx.send(":recycle: All server invites has deleted successfully!")
         else:  # 特定ユーザー分
             target_users = {user.id for user in ctx.message.mentions}
             for invite in await ctx.guild.invites():
                 if invite.inviter.id in target_users:
                     await invite.delete()
             mentions_text = "<@" + "> <@".join(target_users) + ">"
-            await ctx.send(f"All server invites created by {mentions_text[:1900].rsplit('<', 1)[0] + '...' if len(mentions_text) >= 1900 else mentions_text} has deleted successfully!")
+            await ctx.send(f":recycle: All server invites created by {mentions_text[:1900].rsplit('<', 1)[0] + '...' if len(mentions_text) >= 1900 else mentions_text} has deleted successfully!")
 
     @commands.command(aliases=["clear_caches"], usage="clear_cache (@user)", description="Delete invited counts data of mentioned user. If no user mentioned, delete data of all server members.")
     @commands.cooldown(1, 10, commands.BucketType.guild)
@@ -227,7 +227,7 @@ class Invite(commands.Cog):
             for user in self.bot.db[str(ctx.guild.id)]["users"]:
                 self.bot.db[str(ctx.guild.id)]["users"][user]["to"] = set()
                 self.bot.db[str(ctx.guild.id)]["users"][user]["to_all"] = set()
-            await ctx.send("All cached data has deleted successfully!")
+            await ctx.send(":recycle: All cached data has deleted successfully!")
         else:  # 特定ユーザー分
             target_users = []
             for target_user in ctx.message.mentions:
@@ -236,7 +236,7 @@ class Invite(commands.Cog):
                     self.bot.db[str(ctx.guild.id)]["users"][str(target_user.id)]["to"] = set()
                     self.bot.db[str(ctx.guild.id)]["users"][str(target_user.id)]["to_all"] = set()
             mentions_text = "<@" + "> <@".join(target_users) + ">"
-            await ctx.send(f"All cached data of {mentions_text[:1900].rsplit('<', 1)[0] + '...' if len(mentions_text) >= 1900 else mentions_text} has deleted successfully!")
+            await ctx.send(f":recycle: All cached data of {mentions_text[:1900].rsplit('<', 1)[0] + '...' if len(mentions_text) >= 1900 else mentions_text} has deleted successfully!")
 
     def parse_max_uses(self, max_uses: int) -> str:
         if max_uses == 0:
