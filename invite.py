8a569529fbf7c74a8423d44d756e73e394adec40
mafusuke <mafusukee@gmail.com>
2020-10-17 22:52:32 +0900

    Listed problems of invite.py

--- invite.py
+++ invite.py
@@ -165,35 +165,37 @@ class Invite(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
-        if member.id == self.bot.user.id:
+        if member.id == self.bot.user.id:  # BOT自身がサーバーを退出した際の呼び出しを防ぐ
             return
-        if (target_channel := self.bot.db[str(member.guild.id)]["channel"]) is not None:
+        if (target_channel := self.bot.db[str(member.guild.id)]["channel"]) is not None:  # TODO: db対応
             if self.bot.check_permission(member.guild.me):
+                # ログを送信
                 embed = discord.Embed(title=f"{self.bot.datas['emojis']['member_leave']} Member Left", color=0xff1493)
                 embed.set_thumbnail(url=member.avatar_url)
-                # メンバーがデータベース上に存在しないか、招待元がNoneの場合
+                # メンバーがデータベース上に存在しないか、招待元がNoneの場合 # TODO: db対応
                 if (str(member.id) not in self.bot.db[str(member.guild.id)]["users"]) or (self.bot.db[str(member.guild.id)]["users"][str(member.id)]["from"] is None):
                     embed.description = f"<@{member.id}> has left"
                     embed.add_field(name="User", value=f"{member}")
                     embed.add_field(name="Invite", value=f"Unknown")
-                else:
+                else:  # 招待者データがある場合 # TODO: db対応 L2
                     inviter_id = self.bot.db[str(member.guild.id)]["users"][str(member.id)]["from"]
                     invite_code = self.bot.db[str(member.guild.id)]["users"][str(member.id)]["code"]
                     # 招待者がデータに登録されており、招待者の招待先にその人が登録されているなら、現在も入っているメンバーのリストから削除
                     if str(inviter_id) in self.bot.db[str(member.guild.id)]["users"] and member.id in self.bot.db[str(member.guild.id)]["users"][str(inviter_id)]["to"] and member.id in self.bot.db[str(member.guild.id)]["users"][str(inviter_id)]["to_all"]:
-                        self.bot.db[str(member.guild.id)]["users"][str(inviter_id)]["to"].remove(member.id)
+                        self.bot.db[str(member.guild.id)]["users"][str(inviter_id)]["to"].remove(member.id)  # TODO: db対応
                     inviter = await self.catch_user(inviter_id)
                     embed.description = f"<@{member.id}> invited by <@{inviter.id}> has left"
                     embed.add_field(name="User", value=f"{member}")
                     embed.add_field(name="Invite", value=f"{invite_code} | {inviter}")
+                # 滞在した時間を何時間経過したかで表示
                 now = datetime.datetime.now(datetime.timezone.utc)
                 embed.timestamp = now
                 delta = now - pytz.timezone('UTC').localize(member.joined_at)
-                if delta.days == 0:
+                if delta.days == 0:  # 一日以内の場合
                     delta = f"{delta.seconds // 3600}hours {(delta.seconds % 3600) // 60}minutes"
-                elif delta.days <= 7:
+                elif delta.days <= 7:  # 一週間以内の場合
                     delta = f"{delta.days}days {delta.seconds // 3600}hours"
-                else:
+                else:  # それ以上の場合
                     delta = f"{delta.days // 30}months {delta.seconds % 30}days"
                 embed.add_field(name="Stayed Time", value=f"{delta}", inline=False)
                 embed.set_footer(text=f"{member.guild.name} | {len(member.guild.members)}members", icon_url=member.guild.icon_url)
@@ -201,10 +203,17 @@ class Invite(commands.Cog):
 
     @commands.Cog.listener()
     async def check_invite_diff(self, old_invites, new_invites):
+        """
+        招待キャッシュの差異から、招待者を取得
+        :param old_invites: 前の招待キャッシュ
+        :param new_invites: 後の招待キャッシュ
+        :return: [招待者ID:int, 招待コード: str]
+        """
         for invite in old_invites:
             if invite in new_invites:
-                if old_invites[invite] != new_invites[invite]:
-                    return [old_invites[invite]["author"], invite]  # 使用回数が変わっている場合
+                # NOTE: 修正したので正常に動くことを確認する必要がある
+                if old_invites[invite]["uses"] != new_invites[invite]["uses"]:  # 使用回数が変わっている場合
+                    return [old_invites[invite]["author"], invite]
             else:
                 return [old_invites[invite]["author"], invite]  # 招待コードがなくなっている場合→使用上限回数に達した場合
         else:
@@ -212,19 +221,19 @@ class Invite(commands.Cog):
 
     @commands.command(aliases=["inv"], usage="invite (@bot)", description="Show invite URL of this BOT. If bot mentioned, send invite url of mentioned bot")
     async def invite(self, ctx):
-        if not ctx.message.mentions:
+        if not ctx.message.mentions:  # メンションがない場合、このBOTの招待リンクを表示
             await ctx.send(f"__**Add {self.bot.user.name}!**__\n{self.bot.datas['invite']}\n__**Join Official Server!**__\n{self.bot.datas['server']}")
-        else:
+        else:  # メンションがある場合、メンションされたBOTの招待リンクを表示
             invite_text = ""
             for target_user in ctx.message.mentions:
                 if target_user.bot:
                     new_invite_text = f"__**{str(target_user)} 's invite link**__\nhttps://discord.com/oauth2/authorize?client_id={target_user.id}&scope=bot&permissions=-8\n"
-                else:
+                else:  # BOT出ない場合
                     new_invite_text = f"{target_user} is not the bot!\n"
-                if len(invite_text + new_invite_text) > 1900:
+                if len(invite_text + new_invite_text) > 1900:  # 1900文字を超える場合、エラー文を追加して中断
                     invite_text += "(Some invites have been omitted due to message length limit)"
                     break
-                else:
+                else:  # 問題ない場合、末尾に追加
                     invite_text += new_invite_text
             await ctx.send(invite_text)
 
